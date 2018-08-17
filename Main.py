26568de04d5a3aba855aaabb1518279ab53ca0ba
Udit1999 <youditagarwal@gmail.com>
2018-08-17 17:39:43 +0530

    message

--- Main.py
+++ Main.py
@@ -1,19 +1,13 @@
+import pandas as pd
+try:
+    sample = pd.DataFrame.from_csv('sample.csv')
+except:
+    import Sample_generator
+
 import Gmail
 import Classifier
 from  Classifier import predictions
-import pandas as pd
-import tkinter as tk
 
-Inbox = pd.DataFrame.from_csv('Inbox.csv')
-root = tk.Tk()
 
-logo = tk.PhotoImage(file="tenor.gif")
-root.title("New Message Arrived")
-
-msg = tk.Message(root,text = Inbox['Snippets'][0])
-msg.config(bg = 'black',fg = 'white',font = ('times',16))
-msg.pack()
-
-tk.mainloop()
 
 
